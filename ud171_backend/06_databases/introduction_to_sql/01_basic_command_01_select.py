c5c57bc0f46949bf533751888f6a02aa16e111fa
reentrant <sd.engineer@gmail.com>
2019-01-31 20:48:58 -0600

    SELECT statement
    Qualifying Colum name with Table Name

--- ud171_backend/06_databases/introduction_to_sql/01_basic_command_01_select.py
+++ ud171_backend/06_databases/introduction_to_sql/01_basic_command_01_select.py
@@ -8,14 +8,14 @@ contacts = [
 
 # make and populate a table
 db = sqlite3.connect(':memory:')
-db.execute('CREATE TABLE contact ' +
+db.execute('CREATE TABLE person ' +
           '(first_name text, last_name text, e_mail text)')
 
 for contact in contacts:
-    db.execute('INSERT INTO contact VALUES (?, ?, ?);', contact)
+    db.execute('INSERT INTO person VALUES (?, ?, ?);', contact)
 
 def basic_sql_command():
-    cursor = db.execute("SELECT first_name, last_name, e_mail FROM contact;")
+    cursor = db.execute("SELECT person.first_name, person.last_name, person.e_mail FROM person;")
 
     results = []
     for row in cursor:
@@ -24,4 +24,5 @@ def basic_sql_command():
     return results
 
 if __name__ == '__main__':
-    print basic_sql_command()
+    for result in basic_sql_command():
+        print result

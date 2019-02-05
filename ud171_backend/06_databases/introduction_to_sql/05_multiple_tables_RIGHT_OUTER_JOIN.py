7010e567467aebda936c8802dfff59aa6b917340
reentrant <sd.engineer@gmail.com>
2019-02-04 19:57:36 -0600

    Merging multiple tables with JOINs

---
+++ ud171_backend/06_databases/introduction_to_sql/05_multiple_tables_RIGHT_OUTER_JOIN.py
@@ -0,0 +1,66 @@
+from collections import namedtuple
+import sqlite3
+
+# make a basic Person class
+Person = namedtuple('Person', ['first_name', 'last_name'])
+contacts = [
+    Person('Jon', 'Flanders'),
+    Person('Fritz', 'Onion'),
+    Person('Keith', 'Porteous'),
+    Person('Jon', 'McCracken'),
+    Person('Jon', 'Ahern'),
+    Person('James', 'Lifferth'),
+    Person('Brian', 'Curtis')
+    ]
+
+# make and populate a table
+db = sqlite3.connect(':memory:')
+db.execute('CREATE TABLE person ' +
+          '(key integer, first_name text, last_name text)')
+i = 0
+for contact in contacts:
+    i += 1
+    populate_sting = """
+        INSERT INTO person
+        VALUES ({}, '{}', '{}');""".format(i, contact.first_name, contact.last_name)
+    db.execute(populate_sting)
+
+db.execute('CREATE TABLE e_mail_address ' +
+          '(key integer, person_key integer, e_mail text)')
+e_mails_list = [
+    [2, 'fritz@company.com'],
+    [1, 'jon@company.com'],
+    [1, 'jon@another_mail.com']]
+i = 0
+for person_key, e_mail in e_mails_list:
+    i += 1
+    populate_sting = """INSERT INTO e_mail_address VALUES ({}, {}, '{}');
+        """.format(i, person_key, e_mail)
+    db.execute(populate_sting)
+i += 1
+db.execute("INSERT INTO e_mail_address (key, e_mail) VALUES (" + str(i) + ", 'aaron@mail.com');")
+
+def execute_sql_statement():
+    """
+    English question: What are the e-mail addresses I have,
+    including those emails I don't have a person for?
+
+    SELECT p.first_name, p.last_name, e.e_mail
+    FROM person p RIGHT OUTER JOIN e_mail_address e
+    ON p.key = e.person_key;
+    """
+    sql_statement = """
+    SELECT p.first_name, p.last_name, e.e_mail
+    FROM e_mail_address e LEFT OUTER JOIN person p
+    ON p.key = e.person_key;
+    """
+    cursor = db.execute(sql_statement)
+    results = []
+    for row in cursor:
+        results.append(row)
+    return results
+
+
+if __name__ == '__main__':
+    for result in execute_sql_statement():
+        print result

24c616ded26eceb64b62407734c871a6c0c6270a
reentrant <sd.engineer@gmail.com>
2022-07-17 20:10:31 -0500

    Migrate to python3

--- Udac_cs101/04_responding_to_queries/solving_daysBetweenDates.py
+++ Udac_cs101/04_responding_to_queries/solving_daysBetweenDates.py
@@ -1,21 +1,22 @@
-'''
+"""
 Created on 08/01/2018
 
 @author: jruiz
-'''
+"""
 # Credit goes to Websten from forums
 #
 # Use Dave's suggestions to finish your daysBetweenDates
 # procedure. It will need to take into account leap years
 # in addition to the correct number of days in each month.
 
+
 def is_leap_year(year):
-    ''' From wikipedia:
+    """ From wikipedia:
     if (year is not divisible by 4) then (it is a common year)
     else if (year is not divisible by 100) then (it is a leap year)
     else if (year is not divisible by 400) then (it is a common year)
     else (it is a leap year)
-    '''
+    """
     if year % 4 != 0:
         return False
     elif year % 100 != 0:
@@ -25,13 +26,13 @@ def is_leap_year(year):
     else:
         return True
 
+
 def days_in_a_month(year, month):
     days_in_month = [31,28,31,30,31,30,31,31,30,31,30,31]
     if is_leap_year(year) and month == 2:
         return 29
     else:
         return days_in_month[month - 1]
-
 
 
 def nextDay(year, month, day):
@@ -43,7 +44,8 @@ def nextDay(year, month, day):
             return year + 1, 1, 1
         else:
             return year, month + 1, 1
-        
+
+
 def dateIsBefore(year1, month1, day1, year2, month2, day2):
     """Returns True if year1-month1-day1 is before year2-month2-day2. Otherwise, returns False."""
     if year1 < year2:
@@ -55,6 +57,7 @@ def dateIsBefore(year1, month1, day1, year2, month2, day2):
             return day1 < day2
     return False        
 
+
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     """Returns the number of days between year1/month1/day1
        and year2/month2/day2. Assumes inputs are valid dates
@@ -65,9 +68,9 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     while dateIsBefore(year1, month1, day1, year2, month2, day2):
         year1, month1, day1 = nextDay(year1, month1, day1)
         days += 1
-    print "Year: %d " % year1,
-    print days
+    print(f"Year: {year1} {days}")
     return days
+
 
 def main():
     assert nextDay(2016, 2, 28) == (2016, 2, 29)
@@ -80,18 +83,18 @@ def main():
     assert daysBetweenDates(2018, 1, 6, 2018, 1, 6) == 0
     assert daysBetweenDates(2018, 1, 1, 2018, 1, 2) == 1
 
-    test_cases = [((2012,1,1,2012,2,28), 58), 
-                  ((2012,1,1,2012,3,1), 60),
-                  ((2011,6,30,2012,6,30), 366),
-                  ((2011,1,1,2012,8,8), 585 ),
-                  ((1900,1,1,1999,12,31), 36523)]
+    test_cases = [((2012, 1, 1, 2012, 2, 28), 58),
+                  ((2012, 1, 1, 2012, 3, 1), 60),
+                  ((2011, 6, 30, 2012, 6, 30), 366),
+                  ((2011, 1, 1, 2012, 8, 8), 585),
+                  ((1900, 1, 1, 1999, 12, 31), 36523)]
     
     for (args, answer) in test_cases:
         result = daysBetweenDates(*args)
         if result != answer:
-            print "Test with data:", args, "failed"
+            print("Test with data:", args, "failed")
         else:
-            print "Test case passed!"
+            print("Test case passed!")
 
 
 if __name__ == '__main__':
